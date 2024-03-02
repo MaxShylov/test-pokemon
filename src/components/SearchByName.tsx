ba3cb4f3f4709fefd40df6bd43e254980f593078
maxshylov <shma9013@gmail.com>
2024-03-02 21:19:47 +0200

    test2

--- src/components/SearchByName.tsx
+++ src/components/SearchByName.tsx
@@ -7,21 +7,7 @@ interface SearchProps {
 }
 
 export const SearchByName: FC<SearchProps> = () => {
-  const handleSearch = useCallback(
-    (
-      value: string,
-      event?:
-        | React.ChangeEvent<HTMLInputElement>
-        | React.KeyboardEvent<HTMLInputElement>
-        | React.MouseEvent<HTMLElement>,
-      info?: {
-        source?: string | string;
-      },
-    ) => {
-      console.log('value', value);
-    },
-    [],
-  );
+  const handleSearch = useCallback((value: string) => console.warn('value', value), []);
 
   return (
     <Search enterButton="Search" placeholder="input name" allowClear onSearch={handleSearch} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React, { type FC } from 'react';
 
-import { useQuery } from 'src/hooks/useQuery';
 import { useGetPokemonQuery } from 'src/store/services/pokemon';
 
 interface HomeProps {
@@ -8,14 +7,13 @@ interface HomeProps {
 }
 
 export const Home: FC<HomeProps> = () => {
-  const qwe = useQuery();
-  console.log('qwe', qwe);
+  // const qwe = useQuery();
 
   // const { data } = useGetPokemonByNameQuery('bulbasaur');
   const { data: data2 } = useGetPokemonQuery();
 
   // console.log('data', data);
-  console.log('data2', data2);
+  console.warn('data2', data2);
 
   return <div>Home</div>;
 };
